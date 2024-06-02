caab3dd71b07d75baa3a153a83fc778e45cdac50
EliottDup <eliott.duparc@gmail.com>
2024-06-02 16:01:19 +0200

    added a better button!

---
+++ VR-Interactables/Assets/Scripts/ButtonLogic.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ButtonLogic : MonoBehaviour
+{
+    [SerializeField] bool _isToggle = false;
+
+    private bool _value = false;
+    public bool buttonValue
+    {
+        get
+        {
+            return _value;
+        }
+
+        private set
+        {
+            if (value != _value)
+            {
+                if (value)
+                {
+                    OnValueTrue.Invoke();
+                }
+                else
+                {
+                    OnValueFalse.Invoke();
+                }
+                OnValueChanged.Invoke(value);
+            }
+            _value = value;
+        }
+    }
+
+    [Header("Events")]
+    public UnityEvent<bool> OnValueChanged;
+
+    public UnityEvent OnValueTrue, OnValueFalse;
+
+    public void OnPress(BaseInteractionEventArgs args)
+    {
+        if (_isToggle)
+        {
+            buttonValue = !buttonValue;
+        }
+        else
+        {
+            buttonValue = true;
+        }
+    }
+
+    public void OnRelease(BaseInteractionEventArgs args)
+    {
+        if (!_isToggle)
+        {
+            buttonValue = false;
+        }
+    }
+}
